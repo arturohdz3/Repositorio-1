7c7719df4c6bc25b4522e92ad88876a271e0934e
arturohdz3 <40921492+arturohdz3@users.noreply.github.com>
2018-07-07 14:33:30 -0500

    Aplicación 01

---
+++ js/action.js
@@ -0,0 +1,9 @@
+Document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady(){
+	$('#sig1').click(function(){
+		navigator.notification.beep(1);
+		var nombre_usuario = document.getElementByName('nombre')[0].value;
+		localStorage.setItem("nombre", nombre_usuario);
+		navigator.notification.alert("hola "+nombre_usuario, null,"bienvenido","ok");
+	});
+}
